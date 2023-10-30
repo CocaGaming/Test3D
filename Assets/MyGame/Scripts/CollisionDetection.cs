84e788c6529a95e5a24d498726148f3a633a9f7a
CocaGaming <tranngockhuongduy1995@gmail.com>
2023-10-30 21:14:22 +0700

    first commit

---
+++ Assets/MyGame/Scripts/CollisionDetection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+    //Main camera:
+    //Clipping Planes: near, far kcach gần nhất và xa nhất mà camera có thể render dc GO
+    //Culling Mask: render những gì layer nào camera muốn thấy, ví dụ giữa Player và Cây có vật chắn, bỏ layer vật chắn ra Culling Mask
+
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter: " + other.name);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay: " + other.name);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit: " + other.name);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter: " + collision.gameObject.name);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("OnCollisionStay: " + collision.gameObject.name);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("OnCollisionExit: " + collision.gameObject.name);
+    }
+}
