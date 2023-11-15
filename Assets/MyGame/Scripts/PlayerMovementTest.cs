be4bba73e2192140c934a2b14a2bb583a98b8ea5
CocaGaming <tranngockhuongduy1995@gmail.com>
2023-11-15 21:25:06 +0700

    [add] jump animation

--- Assets/MyGame/Scripts/PlayerMovementTest.cs
+++ Assets/MyGame/Scripts/PlayerMovementTest.cs
@@ -9,10 +9,19 @@ public class PlayerMovementTest : MonoBehaviour
     private Animator playerAnim;
     [SerializeField]
     private float moveSpeed;
+
+    [SerializeField]
+    private float jumpSpeed;
+
+    [SerializeField]
+    private float gravityMultiplier;
+
     [SerializeField]
     private float turnSpeed;
+
     [SerializeField]
     private float jumpForce;
+
     [SerializeField]
     private float jumpButtonGracePeriod;//tgian gia hạn khi nv vừa ra khỏi mép vật thể vẫn có thể bấm nhảy dc
 
@@ -22,6 +31,8 @@ public class PlayerMovementTest : MonoBehaviour
     private float originalStepOffset;
     private float? lastGroundedTime;//dấu ? có nghĩa là biến có thể null cũng dc, lần cuối nv đứng trên mặt đất
     private float? jumpButtonPressTime;//tgian bấm nút cách
+    private bool isJumping;
+    private bool isGrounded;
 
 
     private CharacterController characterController;
@@ -45,7 +56,7 @@ public class PlayerMovementTest : MonoBehaviour
 
         float inputMagnitude= Mathf.Clamp01(movementDirection.magnitude);//độ lớn vector
         playerAnim.SetFloat("Input Magnitude", inputMagnitude, 0.05f, Time.deltaTime);//0.05f là damptime: tgian chờ để chuyển anim
-        float speed = inputMagnitude * moveSpeed;
+        //float speed = inputMagnitude * moveSpeed;
 
         movementDirection.Normalize();//chuẩn hóa vector, bth khi đi chéo thì vector chéo sẽ lớn hơn 1 nên đi chéo sẽ nhanh hơn dọc và ngang.
                                       //hàm này giúp vector chéo lúc nào cũng tối đa bằng 1 và = vector dọc và ngang.
@@ -54,7 +65,13 @@ public class PlayerMovementTest : MonoBehaviour
         /*transform.Translate(magnitude* moveSpeed*Time.deltaTime* movementDirection, Space.World);*///dùng để di chuyển những vật ko có vật lý
 
         //yForce là trọng lực của nv
-        yForce += Physics.gravity.y * Time.deltaTime;//physic gravity ban đầu là -9.81, += để nv khi nhảy lên sẽ rơi xuống lại từ từ, ko bị giật hẳn xuống
+        float gravity = Physics.gravity.y * gravityMultiplier;
+
+        if(isJumping && yForce>0 && Input.GetButton("Jump")== false)
+        {
+            gravity *= 2;
+        }
+        yForce += gravity * Time.deltaTime;//physics.gravity.y=-1.0f, += để nv khi nhảy lên sẽ rơi xuống lại từ từ, ko bị giật hẳn xuống
 
         if(characterController.isGrounded)
         {
@@ -70,9 +87,21 @@ public class PlayerMovementTest : MonoBehaviour
         {
             yForce = -0.5f;//chắc chắn nv chạm đất thay vì set =0 có lúc nhảy dc lúc ko
             characterController.stepOffset = originalStepOffset;//chống tình trạng nv bị dính trên tường khi nhảy, lúc nv đang trên đất thì mới so với stepOffset
+            //Đang trên mặt đất
+            playerAnim.SetBool("IsGrounded", true);
+            isGrounded = true;
+
+            playerAnim.SetBool("IsJumping", false);
+            isJumping = false;
+
+            playerAnim.SetBool("IsFalling", false);
             if (Time.time-jumpButtonPressTime<= jumpButtonGracePeriod)//khi bấm nút cách
             {
-                yForce = jumpForce;
+                yForce = Mathf.Sqrt(jumpSpeed* -3.0f * gravity);
+
+                playerAnim.SetBool("IsJumping", true);
+                isJumping = true;
+
                 jumpButtonPressTime = null;
                 lastGroundedTime = null;
             }
@@ -80,6 +109,14 @@ public class PlayerMovementTest : MonoBehaviour
         else
         {
             characterController.stepOffset = 0;//lúc đang nhảy thì nv sẽ ko bị so sánh độ cao với stepOffset
+            //Đang nhảy
+            playerAnim.SetBool("IsGrounded", false);
+            isGrounded = false;
+
+            if((isJumping && yForce < 0) || yForce<-3)//Nhảy lên và đang rớt xuống || đang đi mà lọt hố, số -3 mà lớn quá thì sẽ bị nhầm với đi xuống bậc cầu thang= falling
+            {
+                playerAnim.SetBool("IsFalling", true);
+            }
         }
 
         //Quay hướng nhìn của nv theo hướng di chuyển 
@@ -95,12 +132,23 @@ public class PlayerMovementTest : MonoBehaviour
         {
             playerAnim.SetBool("IsMoving", false);
         }
+
+        if(!isGrounded)
+        {
+            Vector3 velocity =inputMagnitude* jumpSpeed* movementDirection;
+            velocity.y = yForce;
+
+            characterController.Move(velocity * Time.deltaTime);//vì là trong hàm update nên *Time.deltaTime
+        }
     }
     private void OnAnimatorMove()
     {
-        Vector3 velocity = playerAnim.deltaPosition;//hướng * độ lớn = vận tốc, trong Update mode (Animator) normal đã nhân sẵn time.deltatime
-        velocity.y = yForce* Time.deltaTime;//lúc đầu y của direction là 0, phải nhân time.deltatime để đồng bộ với velocity
+        if (isGrounded)
+        {
+            Vector3 velocity = playerAnim.deltaPosition;//trong Update mode (Animator) normal đã nhân sẵn time.deltatime
+            velocity.y = yForce * Time.deltaTime;//phải nhân time.deltatime để đồng bộ với velocity
 
-        characterController.Move(velocity);
+            characterController.Move(velocity * moveSpeed);
+        } 
     }
 }
