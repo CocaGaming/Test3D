6c6dfc7b9303dcb51189dd1448c735a39a96dcfe
CocaGaming <tranngockhuongduy1995@gmail.com>
2023-12-04 21:28:41 +0700

    [add] AI

---
+++ Assets/MyGame/Scripts/EnemyAITest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAITest : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerTF;
+
+    private NavMeshAgent navMeshAgent;
+    private void Awake()
+    {
+        playerTF = GameObject.FindGameObjectWithTag("Player").transform;
+        navMeshAgent=GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(playerTF.position);//di chuyển tới con player và tự tránh các vật cản trên đường
+    }
+}
